24023c80c50dd2fb3e7e245fa3d9896a4caa46da
Cole Inman <coletranemusic@gmail.com>
2015-10-26 22:18:34 +0000

    manufacturing.c updated for phase 2

--- manufacturing.c
+++ manufacturing.c
@@ -31,6 +31,24 @@ state_t* manufacture_complete()
     return &shipping;
 }
 
+void order_init(order_t *ord)
+{
+	if (ord_count == 0)
+	{
+		srandom(time(NULL));
+		ord_count++;
+	}
+	ord->order_size = (random() % 1001) + 1000;
+	ord->capacity = (random() % 41) + 10;
+	ord->duration = (random() % 5) + 1;
+}
+
+void order_process(order_t *ord)
+{
+	sleep(ord->duration);
+	ord_count--;
+}
+
 void entry_to()
 {
     dispatchFactoryLines();
