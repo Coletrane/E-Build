fa2e342ae3797236cb5b8b781edf8cf606ff236c
Cole Inman <coletranemusic@gmail.com>
2015-10-26 22:18:11 +0000

    manufacturing.h updated for phase 2

--- manufacturing.h
+++ manufacturing.h
@@ -3,9 +3,19 @@
 
 #include "state.h"
 
+typedef struct {
+	int order_size;
+	int capacity;
+	int duration;
+} order_t;
+
+int ord_count = 0;
+
 state_t*  manufacture_fail();
 state_t*  manufacture_complete();
 void      entry_to();
 void      exit_from();
+void order_init(order_t *ord);
+void order_process(order_t *ord);
 
 #endif
