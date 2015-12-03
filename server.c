d18062355b4c4467e4000889bad496c26c76f4ed
Cole Inman <coletranemusic@gmail.com>
2015-12-02 22:47:44 -0500

    testing

--- server.c
+++ server.c
@@ -9,7 +9,7 @@
 int main()
 {
 	struct sockaddr_in fsin;
-	msg_t snd;
+	msg_t *snd;
 	msg_t *rcv;
 	unsigned short port = 13;
 	int sock;
@@ -47,12 +47,12 @@ int main()
 
 
 					// Compose and send a message to the client
-					snd.msg_code = 1;
-					snd.param.cap = client[client_count].cap;
-					snd.param.dur = client[client_count].dur;
+					snd->msg_code = 1;
+					snd->param.cap = client[client_count].cap;
+					snd->param.dur = client[client_count].dur;
 					printf("Sending to Client: %d Message Code: 1 (Initialize Client)\n",
 							rcv->client_id);
-					sendto(sock, &snd, sizeof(msg_t), 0, (SA *) &fsin,
+					sendto(sock, snd, sizeof(msg_t), 0, (SA *) &fsin,
 							alen);
 					printf("here\n");
 					break;
@@ -62,26 +62,26 @@ int main()
 							rcv->client_id);
 					if (order_size >= client[rcv->client_id].cap)
 					{
-						snd.num_make = client[rcv->client_id].cap;
+						snd->num_make = client[rcv->client_id].cap;
 						printf("Sending to Client: %d Make Request for %d items\n",
-								rcv->client_id, snd.num_make);
-						sendto(sock, &snd, sizeof(msg_t), 0,
+								rcv->client_id, snd->num_make);
+						sendto(sock, snd, sizeof(msg_t), 0,
 								(SA *) &fsin, alen);
 					}
 					else if (order_size < client[rcv->client_id].cap && order_size > 0)
 					{
-						snd.num_make = order_size;
+						snd->num_make = order_size;
 						printf("Sending to Client: %d Make Request for %d items\n",
-								rcv->client_id, snd.num_make);
-						sendto(sock, &snd, sizeof(msg_t), 0,
+								rcv->client_id, snd->num_make);
+						sendto(sock, snd, sizeof(msg_t), 0,
 								(SA *) &fsin, alen);
 					}
 					else
 					{
-						snd.num_make = 0;
+						snd->num_make = 0;
 						printf("Sending to Client: %d Message Code: 2 Stop Request\n",
 								rcv->client_id);
-						sendto(sock, &snd, sizeof(msg_t), 0,
+						sendto(sock, snd, sizeof(msg_t), 0,
 								(SA *) &fsin, alen);
 					}
 					break;
